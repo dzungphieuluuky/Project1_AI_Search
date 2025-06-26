bdc0c01629d0957c62f41f1e21f56224c80d0ecc
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-06-26 19:10:19 +0700

    add background menu

--- source/main.py
+++ source/main.py
@@ -8,7 +8,7 @@ DARK_BLUE = (0, 80, 200)
 BLACK = (0, 0, 0)
 
 pygame.init()
-WIDTH = 600
+WIDTH = 800
 HEIGHT = 600
 FPS = 60
 
@@ -18,30 +18,47 @@ clock = pygame.time.Clock()
 
 def menu_loop():
     running = True
-    image = pygame.image.load('./img/background.jpg').convert_alpha()
+    image = pygame.image.load('./img/background2.jpg').convert_alpha()
     image_rect = image.get_rect()
     button_width = 200
     button_height = 60
     button_x_coordinate = WIDTH // 2 - button_width // 2
-    buttons = [Button("Play", button_x_coordinate, 220, button_width, button_height, start_game),
-               Button("Instructions", button_x_coordinate, 290, button_width, button_height, instructions),
-               Button("Quit Game", button_x_coordinate, 360, button_width, button_height, pygame.quit)]
+    last_button_y_coordinate = HEIGHT - 20 - button_height
+    buttons = [Button("Play", button_x_coordinate, last_button_y_coordinate - 2 * (button_height + 10), button_width, button_height, start_game),
+               Button("Instructions", button_x_coordinate, last_button_y_coordinate - (button_height + 10), button_width, button_height, instructions),
+               Button("Quit Game", button_x_coordinate, last_button_y_coordinate, button_width, button_height, pygame.quit)]
+    hovered_button = 0
     while running:
         screen.blit(image, image_rect)
-        title = font.render("Main Menu", True, BLACK)
+        title = font.render("Rush Hour Visualizer", True, BLACK)
         screen.blit(title, (WIDTH // 2 - title.get_width() // 2, 100))
+        
+        for button in buttons:
+            button.draw_rect(screen)
 
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
                 running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    buttons[hovered_button].is_hovered = False
+                    hovered_button = (hovered_button - 1) % len(buttons)
+                    buttons[hovered_button].is_hovered = True
+                
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    buttons[hovered_button].is_hovered = False
+                    hovered_button = (hovered_button + 1) % len(buttons)
+                    buttons[hovered_button].is_hovered = True
+                
+                elif event.key == pygame.K_ESCAPE:
+                    running = False
 
             for button in buttons:
                 button.handle_event(event=event)
         
-        for button in buttons:
-            button.draw_rect(screen)
         
         pygame.display.flip()
         clock.tick(FPS)
--- source/menu.py
+++ source/menu.py
@@ -5,8 +5,8 @@ BLUE = (0, 100, 255)
 DARK_BLUE = (0, 80, 200)
 BLACK = (0, 0, 0)
 
-pygame.font.init()
-font = pygame.font.SysFont("Cascadia Mono", 48)
+pygame.init()
+font = pygame.font.SysFont("Cascadia Mono", 96)
 small_font = pygame.font.SysFont("Cascadia Mono", 32)
 
 class Button:
@@ -19,17 +19,25 @@ class Button:
         self.normal_color = BLUE
         self.hovered_color = DARK_BLUE
         self.callback = callback
-        self.hover = False
+        self.is_hovered = False
+        self.text_surface = None
 
     def draw_rect(self, surface):
-        color = self.hovered_color if self.hover else self.normal_color
+        self.surface = surface
+        color = self.hovered_color if self.is_hovered else self.normal_color
         pygame.draw.rect(surface, color, self.rect)
         text_surf = small_font.render(self.text, True, WHITE)
+        self.text_surface = text_surf
         text_rect = text_surf.get_rect(center=self.rect.center)
         surface.blit(text_surf, text_rect)
+        if self.is_hovered:
+            pygame.transform.smoothscale_by(text_surf, 1.1)
+
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEMOTION:
-            self.hover = self.rect.collidepoint(event.pos)
-        elif event.type == pygame.MOUSEBUTTONDOWN and self.hover:
+            self.is_hovered = self.rect.collidepoint(event.pos)
+        elif self.is_hovered and (event.type == pygame.MOUSEBUTTONDOWN or
+                                  event.type == pygame.KEYDOWN and (event.key == pygame.K_RETURN or
+                                                                    event.key == pygame.K_SPACE)):
             self.callback()
