f30dd7d22a81104d64da39265b02c3620ed8113e
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-06-28 11:13:35 +0700

    add expand animation for button when hovering

--- source/menu.py
+++ source/menu.py
@@ -15,21 +15,18 @@ class Button:
         self.rect = pygame.Rect(x, y, width, height)
         self.text = text
         self.color = color
-        self.button_rect = None
         # callback function to call the function of the button
         self.callback = callback
 
         self.is_hovered = False
 
     def draw_button(self, surface):
-        if self.button_rect is None:
-            self.button_rect = pygame.draw.rect(surface, self.color, self.rect, border_radius=15)
-        else: 
-            pygame.draw.rect(surface, self.color, self.rect, border_radius=15)
         text_surf = small_font.render(self.text, True, WHITE)
         if self.is_hovered:
             text_surf = pygame.transform.scale_by(text_surf, 1.1)
-            self.button_rect.scale_by_ip(1.1, 1.1)
+            pygame.draw.rect(surface, self.color, self.rect.scale_by(1.1, 1.1), border_radius=15)
+        else:
+            pygame.draw.rect(surface, self.color, self.rect, border_radius=15)
         text_rect = text_surf.get_rect(center=self.rect.center)
         surface.blit(text_surf, text_rect)
 
