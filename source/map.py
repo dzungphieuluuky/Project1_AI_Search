27921d735c7410348410a18405607a979875a10b
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-07-01 21:26:10 +0700

    add maps

---
+++ source/map.py
@@ -0,0 +1,103 @@
+map0 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+}
+
+map1 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+}
+
+map2 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (3, 3)},
+    'D': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+}
+
+map3 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (3, 3)},
+    'D': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (2, 2)},
+}
+
+map4 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 1)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (3, 3)},
+    'D': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 2)},
+    'F': {'orientation': 'V', 'cost': 3, 'position': (0, 0)},
+}
+
+map5 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 1)},
+    'A': {'orientation': 'H', 'cost': 3, 'position': (1, 1)},
+    'B': {'orientation': 'H', 'cost': 2, 'position': (3, 0)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (5, 1)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (1, 0)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 2)},
+    'F': {'orientation': 'V', 'cost': 2, 'position': (2, 3)},
+    'G': {'orientation': 'V', 'cost': 2, 'position': (4, 0)},
+    'H': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+}
+
+map6 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 2)},
+    'C': {'orientation': 'V', 'cost': 2, 'position': (1, 4)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (3, 4)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 1)},
+    'F': {'orientation': 'H', 'cost': 2, 'position': (4, 2)},
+}
+
+
+map7 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 3)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 2)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (1, 5)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (3, 0)},
+    'D': {'orientation': 'H', 'cost': 2, 'position': (4, 4)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 3)},
+}
+
+map8 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 1)},
+    'A': {'orientation': 'H', 'cost': 3, 'position': (1, 1)},
+    'B': {'orientation': 'H', 'cost': 2, 'position': (3, 0)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (1, 0)},
+    'E': {'orientation': 'V', 'cost': 3, 'position': (3, 2)},
+    'F': {'orientation': 'V', 'cost': 2, 'position': (2, 3)},
+
+}
+
+
+map9 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 2)},
+    'C': {'orientation': 'V', 'cost': 2, 'position': (1, 4)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (3, 4)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 1)},
+    'F': {'orientation': 'H', 'cost': 2, 'position': (4, 2)},
+    'G': {'orientation': 'H', 'cost': 2, 'position': (0, 4)},
+}
+
+map10 = {
+    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (1, 2)},
+    'C': {'orientation': 'V', 'cost': 3, 'position': (1, 4)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (4, 1)},
+    'E': {'orientation': 'H', 'cost': 2, 'position': (3, 0)},
+    'F': {'orientation': 'H', 'cost': 2, 'position': (5, 2)},
+}
+maps = [map0, map1, map2, map3, map4, map5,
+        map6, map7, map8, map9, map10]
