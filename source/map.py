b332dea0568e60cd58ac326e324b5b87b6db2b9e
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-07-03 11:02:20 +0700

    edit maps

--- source/map.py
+++ source/map.py
@@ -1,113 +1,100 @@
-maps = []
-
 map0 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (0, 2)},
 }
-maps.append(map0)
 
 map1 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (0, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (3, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (5, 0)},
 }
-maps.append(map1)
 
 map2 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (0, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (3, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (5, 0)},
     'C': {'orientation': 'H', 'cost': 3, 'position': (3, 3)},
-    'D': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+    'D': {'orientation': 'H', 'cost': 3, 'position': (3, 4)},
 }
-maps.append(map2)
 
 map3 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (0, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (3, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (5, 0)},
     'C': {'orientation': 'H', 'cost': 3, 'position': (3, 3)},
-    'D': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+    'D': {'orientation': 'H', 'cost': 3, 'position': (3, 4)},
     'E': {'orientation': 'V', 'cost': 2, 'position': (2, 2)},
 }
-maps.append(map3)
 
 map4 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 1)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (1, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (3, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (5, 0)},
     'C': {'orientation': 'H', 'cost': 3, 'position': (3, 3)},
-    'D': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
-    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 2)},
+    'D': {'orientation': 'H', 'cost': 3, 'position': (3, 4)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (2, 3)},
     'F': {'orientation': 'V', 'cost': 3, 'position': (0, 0)},
 }
-maps.append(map4)
 
 map5 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 1)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (1, 2)},
     'A': {'orientation': 'H', 'cost': 3, 'position': (1, 1)},
-    'B': {'orientation': 'H', 'cost': 2, 'position': (3, 0)},
-    'C': {'orientation': 'H', 'cost': 3, 'position': (5, 1)},
-    'D': {'orientation': 'V', 'cost': 2, 'position': (1, 0)},
-    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 2)},
-    'F': {'orientation': 'V', 'cost': 2, 'position': (2, 3)},
-    'G': {'orientation': 'V', 'cost': 2, 'position': (4, 0)},
-    'H': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
+    'B': {'orientation': 'H', 'cost': 2, 'position': (0, 3)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (1, 5)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (0, 1)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (2, 3)},
+    'F': {'orientation': 'V', 'cost': 2, 'position': (3, 2)},
+    'G': {'orientation': 'V', 'cost': 2, 'position': (0, 4)},
+    'H': {'orientation': 'H', 'cost': 3, 'position': (3, 4)},
 }
-maps.append(map5)
 
 map6 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 2)},
-    'C': {'orientation': 'V', 'cost': 2, 'position': (1, 4)},
-    'D': {'orientation': 'V', 'cost': 2, 'position': (3, 4)},
-    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 1)},
-    'F': {'orientation': 'H', 'cost': 2, 'position': (4, 2)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (0, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (3, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (2, 0)},
+    'C': {'orientation': 'V', 'cost': 2, 'position': (4, 1)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (4, 3)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (1, 3)},
+    'F': {'orientation': 'H', 'cost': 2, 'position': (2, 4)},
 }
-maps.append(map6)
 
 map7 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 3)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 2)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (1, 5)},
-    'C': {'orientation': 'H', 'cost': 3, 'position': (3, 0)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (3, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (2, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (5, 1)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (0, 3)},
     'D': {'orientation': 'H', 'cost': 2, 'position': (4, 4)},
     'E': {'orientation': 'V', 'cost': 2, 'position': (3, 3)},
 }
-maps.append(map7)
 
 map8 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 1)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (1, 2)},
     'A': {'orientation': 'H', 'cost': 3, 'position': (1, 1)},
-    'B': {'orientation': 'H', 'cost': 2, 'position': (3, 0)},
-    'C': {'orientation': 'H', 'cost': 3, 'position': (4, 3)},
-    'D': {'orientation': 'V', 'cost': 2, 'position': (1, 0)},
-    'E': {'orientation': 'V', 'cost': 3, 'position': (3, 2)},
-    'F': {'orientation': 'V', 'cost': 2, 'position': (2, 3)},
-
+    'B': {'orientation': 'H', 'cost': 2, 'position': (0, 3)},
+    'C': {'orientation': 'H', 'cost': 3, 'position': (3, 4)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (0, 1)},
+    'E': {'orientation': 'V', 'cost': 3, 'position': (2, 3)},
+    'F': {'orientation': 'V', 'cost': 2, 'position': (3, 2)},
 }
-maps.append(map8)
-
 
 map9 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 3)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (0, 2)},
-    'C': {'orientation': 'V', 'cost': 2, 'position': (1, 4)},
-    'D': {'orientation': 'V', 'cost': 2, 'position': (3, 4)},
-    'E': {'orientation': 'V', 'cost': 2, 'position': (3, 1)},
-    'F': {'orientation': 'H', 'cost': 2, 'position': (4, 2)},
-    'G': {'orientation': 'H', 'cost': 2, 'position': (0, 4)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (0, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (3, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (2, 0)},
+    'C': {'orientation': 'V', 'cost': 2, 'position': (4, 1)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (4, 3)},
+    'E': {'orientation': 'V', 'cost': 2, 'position': (1, 3)},
+    'F': {'orientation': 'H', 'cost': 2, 'position': (2, 4)},
+    'G': {'orientation': 'H', 'cost': 2, 'position': (4, 0)},
 }
-maps.append(map9)
 
 map10 = {
-    'player': {'orientation': 'H', 'cost': 2, 'position': (2, 0)},
-    'A': {'orientation': 'V', 'cost': 3, 'position': (0, 5)},
-    'B': {'orientation': 'V', 'cost': 3, 'position': (1, 2)},
-    'C': {'orientation': 'V', 'cost': 3, 'position': (1, 4)},
-    'D': {'orientation': 'V', 'cost': 2, 'position': (4, 1)},
-    'E': {'orientation': 'H', 'cost': 2, 'position': (3, 0)},
-    'F': {'orientation': 'H', 'cost': 2, 'position': (5, 2)},
+    'player': {'orientation': 'H', 'cost': 2, 'position': (0, 2)},
+    'A': {'orientation': 'V', 'cost': 3, 'position': (5, 0)},
+    'B': {'orientation': 'V', 'cost': 3, 'position': (2, 1)},
+    'C': {'orientation': 'V', 'cost': 3, 'position': (4, 1)},
+    'D': {'orientation': 'V', 'cost': 2, 'position': (1, 4)},
+    'E': {'orientation': 'H', 'cost': 2, 'position': (0, 3)},
+    'F': {'orientation': 'H', 'cost': 2, 'position': (2, 5)},
 }
-maps.append(map10)
+maps = [map0, map1, map2, map3, map4, map5,
+        map6, map7, map8, map9, map10]
