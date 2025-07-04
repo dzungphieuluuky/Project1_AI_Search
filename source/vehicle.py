78c5c25efd9ac7dcbe0a34afefb5034a7e012f80
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-07-04 22:18:35 +0700

    add data type for vehicle class

--- source/vehicle.py
+++ source/vehicle.py
@@ -1,26 +1,32 @@
 import pygame
 import os
 class Vehicle:
-    def __init__(self, id, col, row, length, orientation):
+    def __init__(self, id: str, col: int, row: int, length: int, orientation: str) -> None:
         self.id = id 
         self.col = col  
         self.row = row                               
         self.length = length       
         self.orientation = orientation
-    def get_id(self):
+
+    def get_id(self) -> str:
         return self.id
-    def get_position(self):
+    
+    def get_position(self) -> tuple[int, int]:
         return self.row, self.col
-    def get_length(self):
+    
+    def get_length(self) -> int:
         return self.length
-    def get_orientation(self):
+    
+    def get_orientation(self) -> str:
         return self.orientation
-    def move(self, delta):
+    
+    def move(self, delta) -> None:
         if self.orientation == 'H':
             self.col += delta
         else:
             self.row += delta
-    def get_occupied_cells(self):
+
+    def get_occupied_cells(self) -> list[tuple[int, int]]:
         cells = []
         for i in range(self.length):
             if self.orientation == 'H':
@@ -28,9 +34,11 @@ class Vehicle:
             else:
                 cells.append((self.col, self.row + i))
         return cells
+    
 class Car(Vehicle):
-    def __init__(self, id, col, row, orientation):
+    def __init__(self, id: str, col: int, row: int, orientation: str) -> None:
         super().__init__(id, col, row, 2, orientation)
+
     def draw(self, folder_path: str, grid_size: int) -> pygame.Surface:
         if self.id == "player":
             filename = "player.png"
@@ -47,9 +55,11 @@ class Car(Vehicle):
         image_path = os.path.join(folder_path, filename)
         image = pygame.image.load(image_path).convert_alpha()
         return pygame.transform.scale(image, (width, height))
+    
 class Truck(Vehicle):
-    def __init__(self, id, col, row, orientation):
+    def __init__(self, id: str, col: int, row: int, orientation: str) -> None:
         super().__init__(id, col, row, 3, orientation)
+
     def draw(self, folder_path: str, grid_size: int) -> pygame.Surface:
         if self.orientation == 'H':
             filename = "horizontal_truck.png"
